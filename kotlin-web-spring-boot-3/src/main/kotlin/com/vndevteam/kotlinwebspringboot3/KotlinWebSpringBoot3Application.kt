840a3057d5bb250c268f3af0f86bc8ee2125f282
Lam Ngoc Khuong <me@ngockhuong.com>
2023-10-03 22:13:55 +0700

    Init kotlin-web-spring-boot-3 project

---
+++ kotlin-web-spring-boot-3/src/main/kotlin/com/vndevteam/kotlinwebspringboot3/KotlinWebSpringBoot3Application.kt
@@ -0,0 +1,11 @@
+package com.vndevteam.kotlinwebspringboot3
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinWebSpringBoot3Application
+
+fun main(args: Array<String>) {
+    runApplication<KotlinWebSpringBoot3Application>(*args)
+}
---
+++ kotlin-web-spring-boot-3/src/test/kotlin/com/vndevteam/kotlinwebspringboot3/KotlinWebSpringBoot3ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.vndevteam.kotlinwebspringboot3
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinWebSpringBoot3ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
