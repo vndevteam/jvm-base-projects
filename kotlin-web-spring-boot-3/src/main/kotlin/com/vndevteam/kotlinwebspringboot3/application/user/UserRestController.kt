326d812925677801295edbc3ea8ef38e19f0b7d2
Lam Ngoc Khuong <me@ngockhuong.com>
2023-10-24 12:42:13 +0700

    MessageSource config

--- kotlin-web-spring-boot-3/src/main/kotlin/com/vndevteam/kotlinwebspringboot3/application/user/UserRestController.kt
+++ kotlin-web-spring-boot-3/src/main/kotlin/com/vndevteam/kotlinwebspringboot3/application/user/UserRestController.kt
@@ -1,7 +1,9 @@
 package com.vndevteam.kotlinwebspringboot3.application.user
 
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-@RestController("/users")
+@RestController
+@RequestMapping("/users")
 class UserRestController {
 }
---
+++ kotlin-web-spring-boot-3/src/main/kotlin/com/vndevteam/kotlinwebspringboot3/infrastructure/config/DatabaseConfig.kt
@@ -0,0 +1,4 @@
+package com.vndevteam.kotlinwebspringboot3.infrastructure.config
+
+class DatabaseConfig {
+}
