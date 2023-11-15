30890e379d70d26e32d239fd532eb528de0bc004
Lam Ngoc Khuong <lam.ngoc.khuong@sun-asterisk.com>
2023-11-15 09:31:24 +0700

    Upgrade dependences version

--- kotlin-web-spring-boot-3-auth/build.gradle.kts
+++ kotlin-web-spring-boot-3-auth/build.gradle.kts
@@ -2,10 +2,12 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     id("org.springframework.boot") version "3.1.5"
-    id("io.spring.dependency-management") version "1.1.3"
-    kotlin("jvm") version "1.8.22"
-    kotlin("plugin.spring") version "1.8.22"
-    kotlin("plugin.jpa") version "1.8.22"
+    id("io.spring.dependency-management") version "1.1.4"
+    id("com.diffplug.spotless") version "6.22.0"
+    id("com.google.cloud.tools.jib") version "3.4.0"
+    kotlin("jvm") version "1.9.20"
+    kotlin("plugin.spring") version "1.9.20"
+    kotlin("plugin.jpa") version "1.9.20"
 }
 
 group = "com.vndevteam"
@@ -41,6 +43,18 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+spotless {
+    kotlin {
+        ktfmt("0.46").kotlinlangStyle()
+    }
+}
+
+jib {
+    from {
+        image = "eclipse-temurin:17-jre-alpine"
+    }
 }
 
 tasks.bootBuildImage {
