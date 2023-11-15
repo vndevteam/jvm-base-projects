928cc6dd5c0642b0f3f1997a43a1fcd33d5727ea
Lam Ngoc Khuong <lam.ngoc.khuong@sun-asterisk.com>
2023-11-15 09:40:35 +0700

    Fix lint

--- kotlin-web-spring-boot-3-auth/src/test/kotlin/com/vndevteam/kotlinwebspringboot3auth/KotlinWebSpringBoot3AuthApplicationTests.kt
+++ kotlin-web-spring-boot-3-auth/src/test/kotlin/com/vndevteam/kotlinwebspringboot3auth/KotlinWebSpringBoot3AuthApplicationTests.kt
@@ -1,8 +1,5 @@
 package com.vndevteam.kotlinwebspringboot3auth
 
-import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
-class KotlinWebSpringBoot3AuthApplicationTests {
-}
+@SpringBootTest class KotlinWebSpringBoot3AuthApplicationTests {}
