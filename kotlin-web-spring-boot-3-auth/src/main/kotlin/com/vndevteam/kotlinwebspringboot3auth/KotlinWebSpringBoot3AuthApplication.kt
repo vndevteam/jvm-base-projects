c41d95cacdda23f44aaa90bbfb17ee59c2a2775e
Lam Ngoc Khuong <lam.ngoc.khuong@sun-asterisk.com>
2023-11-15 09:25:12 +0700

    Add new module: kotlin-web-spring-boot-3-auth

---
+++ kotlin-web-spring-boot-3-auth/src/main/kotlin/com/vndevteam/kotlinwebspringboot3auth/KotlinWebSpringBoot3AuthApplication.kt
@@ -0,0 +1,11 @@
+package com.vndevteam.kotlinwebspringboot3auth
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinWebSpringBoot3AuthApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinWebSpringBoot3AuthApplication>(*args)
+}
---
+++ kotlin-web-spring-boot-3-auth/src/test/kotlin/com/vndevteam/kotlinwebspringboot3auth/KotlinWebSpringBoot3AuthApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.vndevteam.kotlinwebspringboot3auth
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinWebSpringBoot3AuthApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
