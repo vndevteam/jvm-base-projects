107ef8935ab2df370edc93147b058993550ccedb
Lam Ngoc Khuong <lam.ngoc.khuong@sun-asterisk.com>
2024-04-12 10:15:30 +0700

    feat(java-web-spring-boot-3): add new module

---
+++ java-web-spring-boot-3/src/test/java/com/vndevteam/javawebspringboot3/JavaWebSpringBoot3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vndevteam.javawebspringboot3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaWebSpringBoot3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
